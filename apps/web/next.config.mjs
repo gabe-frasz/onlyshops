9527e1c61062f3af6651ee834515022f71f4ece0
Gabriel <gabrielvitor.frasao@gmail.com>
2023-01-15 18:45:15 -0300

    feat: inital commit

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("next").NextConfig} */
+
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+export default config;
---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next App</title>
+        <meta name="description" content="Next App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div></div>
+    </>
+  );
+};
+
+export default Home;
