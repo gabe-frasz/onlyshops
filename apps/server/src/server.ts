8a860243d9c94ab27a08060dd2c365cec64c193b
Gabriel <gabrielvitor.frasao@gmail.com>
2023-01-19 16:02:50 -0300

    refactor: fix env variables, add prefix to routes

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,19 +1,22 @@
-import { OAuth2Provider } from "@/@types";
-import { authRoutes, checkoutRoutes } from "@/routes";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
 import oauth2 from "@fastify/oauth2";
 import Fastify from "fastify";
+import rawBody from "fastify-raw-body";
+
+import { OAuth2Provider } from "@/@types";
+import { authRoutes, checkoutRoutes, webhookRoutes } from "@/routes";
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  const PORT = (process.env.PORT || 5000) as number;
+  const PORT = Number(process.env.PORT || 5000);
 
-  await fastify.register(cors, { origin: process.env.CORS_ORIGIN });
+  await fastify.register(cors, { origin: process.env.WEB_APP_ORIGIN });
   await fastify.register(jwt, { secret: process.env.JWT_SECRET! });
+  await fastify.register(rawBody, { global: false });
   await fastify.register(oauth2, {
     name: "github" as OAuth2Provider,
     credentials: {
@@ -29,7 +32,8 @@ async function bootstrap() {
   });
 
   await fastify.register(authRoutes);
-  await fastify.register(checkoutRoutes);
+  await fastify.register(checkoutRoutes, { prefix: "/checkouts" });
+  await fastify.register(webhookRoutes, { prefix: "/webhooks" });
 
   await fastify.listen({ port: PORT });
 }
